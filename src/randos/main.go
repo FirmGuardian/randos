41f664e5cefafac3974bf5899c85bcb21e193c87
Liam Conrad <liam@storskegg.org>
2017-10-18 18:55:44 -0500

    Add prompt and info

--- src/randos/main.go
+++ src/randos/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"bufio"
 	"crypto/rand"
 	"crypto/sha512"
 	"encoding/hex"
 	"fmt"
 	"github.com/CrowdSurge/banner"
+	"github.com/urfave/cli"
 	"os"
+	"strconv"
 )
 
 const (
@@ -19,8 +22,6 @@ type finfo struct {
 	name string
 	size uint32
 }
-
-var throttle = make(chan int)
 
 var files = []finfo{
 	{
@@ -58,31 +59,78 @@ var files = []finfo{
 }
 
 func main() {
-	banner.Print("rando")
-	fmt.Println(sumOfSizes(files))
+	app := cli.NewApp()
+	app.Name = "rando"
+	app.Version = "1.0.0"
+	app.Usage = "Random data file generator"
 
-	fmt.Println("Creating benchmark files...")
+	app.Action = func(c *cli.Context) error {
+		banner.Print("rando")
 
-	// our checksum file, for matching convenience
-	sumFile, _ := os.Create("./CHECKSUM.SHA512-" + prefix)
-	defer sumFile.Close()
+		sizeFiles := strconv.Itoa(sumOfSizes(files))
+		numFiles := strconv.Itoa(len(files))
 
-	for i, file := range files {
-		sum := makeFile(i, file.name, file.size)
-		// The beginning to a thought. It's incomplete, and not helping or hurting anything.
-		go func() {
-			throttle <- 1
-			// generate sha256, and write it async
-			hashLine := "SHA512 (" + prefix + "-" + file.name + ext + ") = " + sum + "\n"
-			sumFile.WriteString(hashLine)
+		keepGoing := promptABTest(
+			"Rando will generate "+numFiles+" files totaling "+sizeFiles+" MB. Continue? (y/n)",
+			"y",
+			"n",
+		)
 
-			// flush the buffers
-			sumFile.Sync()
-			<-throttle
-		}()
+		if keepGoing {
+			fmt.Println("Creating benchmark files...")
+
+			// our checksum file, for matching convenience
+			sumFile, err := os.Create("./CHECKSUM.SHA512-" + prefix)
+			if err != nil {
+				return err
+			}
+			defer sumFile.Close()
+
+			for i, file := range files {
+				checksum, err := makeFile(i, file.name, file.size)
+				if err != nil {
+					return err
+				}
+
+				// generate sha256, and write it async
+				hashLine := "SHA512 (" + prefix + "-" + file.name + ext + ") = " + checksum + "\n"
+				sumFile.WriteString(hashLine)
+
+				// flush the buffers
+				sumFile.Sync()
+			}
+
+			fmt.Println("All done!")
+		} else {
+			fmt.Println("\nOK, bye.")
+		}
+
+		return nil
 	}
+	app.Run(os.Args)
+}
 
-	fmt.Println("All done!")
+func prompt(inquiry string) string {
+	reader := bufio.NewReader(os.Stdin)
+	if inquiry[len(inquiry)-1:] == "\n" {
+		print(inquiry)
+	} else {
+		print(inquiry + " ")
+	}
+	text, _ := reader.ReadString('\n')
+	return text[0 : len(text)-1]
+}
+
+func promptABTest(inquiry string, a string, b string) bool {
+	response := prompt(inquiry)
+	if response == a {
+		return true
+	} else if response == b {
+		return false
+	} else {
+		panic("Don't understand what you said. Bye!")
+	}
+	return false
 }
 
 func sumOfSizes(sizes []finfo) int {
@@ -95,30 +143,39 @@ func sumOfSizes(sizes []finfo) int {
 	return int(n / onemeg)
 }
 
-func makeFile(idx int, name string, sz uint32) string {
+func makeFile(idx int, name string, sz uint32) (string, error) {
 	path := "./" + prefix + "-" + name + ext
-	fmt.Println(idx+1, "Creating file: "+path)
+	fmt.Print(idx+1, " Creating file: "+path+"...")
 
-	f, _ := os.Create(path)
+	f, err := os.Create(path)
+	if err != nil {
+		return "", err
+	}
 	defer f.Close()
 
 	// generate random data
 	bytes := make([]byte, sz)
 	rand.Read(bytes)
-	n, _ := f.Write(bytes)
+	n, err := f.Write(bytes)
+	if err != nil {
+		return "", err
+	}
 
 	// Flush those buffers
 	f.Sync()
 	fmt.Println(n, "bytes written")
 
-	return generateHash(bytes)
+	return generateHash(bytes), nil
 }
 
 func generateHash(bytes []byte) string {
+	fmt.Print("Generating hash...")
 	// looks like checksum files are still using FIPS 180-2. someday
 	// we'll move on to the lovely FIPS 202
 	//hash := sha3.New512() // FIPs 202
 	hash := sha512.New() // FIPS 180-2
 	hash.Write(bytes)
-	return hex.EncodeToString(hash.Sum(nil))
+	hashString := hex.EncodeToString(hash.Sum(nil))
+	fmt.Println("Done.")
+	return hashString
 }
